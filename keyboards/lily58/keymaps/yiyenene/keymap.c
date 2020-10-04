e5b57f00f860e8d5271b4fe1fa067cdaac583864
yiyenene <goodbyeboredworld@aol.jp>
2020-10-04 21:54:19 +0900

    modify keymaps

--- keyboards/lily58/keymaps/yiyenene/keymap.c
+++ keyboards/lily58/keymaps/yiyenene/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  ESC  |    |  Del  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  |  JP  |  EN  | RAISE|
+ *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  | RAISE|  EN  |  JP  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_ESC,   KC_DEL,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             LOWER, KC_LALT,KC_LGUI, KC_SPC,   KC_ENT,   KC_LANG1,KC_LANG2, RAISE \
+                             LOWER, KC_LALT,KC_LGUI, KC_SPC,   KC_ENT,   RAISE,   KC_LANG1,KC_LANG2  \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  ESC  |    |  Del  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |   [  |   ]  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  |  JP  |  EN  | RAISE|
+ *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  | RAISE|  EN  |  JP  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -82,11 +82,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|   *  |   /  |      |      |  Up  |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| Left | Down |  Up  | Right|  Up  |      |
  * |------+------+------+------+------+------|  ESC  |    |  Del  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  | Left | Down | Right|
+ * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|      |      |      | Left | Down | Right|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  |  JP  |  EN  | RAISE|
+ *                   |LOWER | LALT | LGUI | /Space  /       \Enter \  | RAISE|  EN  |  JP  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,   KC_8,   KC_9,   KC_0,    _______, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_ASTR, KC_SLSH, XXXXXXX, XXXXXXX ,   KC_UP, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LEFT, KC_DOWN, KC_RIGHT, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_UP, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX,  KC_LEFT, KC_DOWN, KC_RIGHT, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
